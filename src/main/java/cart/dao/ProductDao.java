06b5c317074df5af7d0bc6a1dba1be0c1560f0bf
kdkdhoho <hkim4410@naver.com>
2023-04-27 16:47:49 +0900

    feat: id로 단일 값 조회 기능 구현

--- src/main/java/cart/dao/ProductDao.java
+++ src/main/java/cart/dao/ProductDao.java
@@ -1,14 +1,24 @@
 package cart.dao;
 
-import cart.domain.Product;
+import cart.entity.ProductEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ProductDao {
+
+    private static final RowMapper<ProductEntity> productRowMapper = (resultSet, rowNum) -> new ProductEntity(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getInt("price"),
+            resultSet.getString("image_url")
+    );
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
@@ -20,9 +30,20 @@ public class ProductDao {
                 .usingGeneratedKeyColumns("id");
     }
 
-    public long insert(final Product product) {
-        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(product);
+    public long insert(final ProductEntity productEntity) {
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(productEntity);
 
         return simpleJdbcInsert.executeAndReturnKey(sqlParameterSource).longValue();
+    }
+
+    public Optional<ProductEntity> findById(final long id) {
+        String sql = "select id, name, price, image_url from Product where id = ?";
+
+        List<ProductEntity> result = jdbcTemplate.query(sql, productRowMapper, id);
+
+        if (result.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(result.get(0));
     }
 }
--- src/test/java/cart/dao/ProductDaoTest.java
+++ src/test/java/cart/dao/ProductDaoTest.java
@@ -1,15 +1,20 @@
 package cart.dao;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import cart.domain.Product;
+import cart.entity.ProductEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import java.util.Optional;
 
 @JdbcTest
+@Sql("classpath:schema.sql")
 class ProductDaoTest {
 
     private ProductDao productDao;
@@ -20,14 +25,44 @@ class ProductDaoTest {
     @BeforeEach
     void beforeEach() {
         this.productDao = new ProductDao(jdbcTemplate);
+
+        settingDummyData();
+    }
+
+    private void settingDummyData() {
+        String sql1 = "INSERT INTO Product (name, price, image_url) values ('상품1', 1000, '상품1 이미지');";
+        String sql2 = "INSERT INTO Product (name, price, image_url) values ('상품2', 2000, '상품2 이미지');";
+
+        jdbcTemplate.execute(sql1 + sql2);
     }
 
     @Test
-    void 상품_추가_성공() {
-        Product product = new Product("상품", 1000, "image");
+    void 추가_성공() {
+        ProductEntity productEntity = new ProductEntity("상품", 1000, "image");
 
         assertThatNoException().isThrownBy(
-                () -> productDao.insert(product)
+                () -> productDao.insert(productEntity)
+        );
+    }
+
+    @Test
+    void 존재하지_않는_상품_조회시_Optional에_null이_포함되어_반환된다() {
+        Optional<ProductEntity> result = productDao.findById(3);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void 조회_성공() {
+        Optional<ProductEntity> result = productDao.findById(1);
+
+        assertThat(result.isPresent()).isTrue();
+
+        ProductEntity foundProductEntity = result.get();
+        assertAll(
+                () -> assertThat(foundProductEntity.getName()).isEqualTo("상품1"),
+                () -> assertThat(foundProductEntity.getPrice()).isEqualTo(1000),
+                () -> assertThat(foundProductEntity.getImageUrl()).isEqualTo("상품1 이미지")
         );
     }
 }
