33b63387269869cf0d24b395f8f28d7327bebe49
kdkdhoho <hkim4410@naver.com>
2023-04-27 11:15:23 +0900

    refactor: Product에 롬복 적용 및 생성자 수정

--- src/main/java/cart/domain/Product.java
+++ src/main/java/cart/domain/Product.java
@@ -1,32 +1,19 @@
 package cart.domain;
 
+import lombok.Getter;
+
+@Getter
 public class Product {
 
-    private final int id;
+    private final Integer id;
     private final String name;
     private final int price;
     private final String imageUrl;
 
-    public Product(final int id, final String name, final int price, final String imageUrl) {
-        this.id = id;
+    public Product(final String name, final int price, final String imageUrl) {
+        id = null;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
     }
 }
