7096b2d8ae0b37c875ef5174281328a58b879ba1
kdkdhoho <hkim4410@naver.com>
2023-04-27 16:46:43 +0900

    refactor: Product를 도메인에서 엔티티로 변경

--- src/main/java/cart/entity/ProductEntity.java
+++ src/main/java/cart/entity/ProductEntity.java
@@ -1,23 +1,23 @@
-package cart.domain;
+package cart.entity;
 
 import lombok.Getter;
 
 @Getter
-public class Product {
+public class ProductEntity {
 
     private final Integer id;
     private final String name;
     private final int price;
     private final String imageUrl;
 
-    public Product(final Integer id, final String name, final int price, final String imageUrl) {
+    public ProductEntity(final int id, final String name, final int price, final String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
     }
 
-    public Product(final String name, final int price, final String imageUrl) {
+    public ProductEntity(final String name, final int price, final String imageUrl) {
         id = null;
         this.name = name;
         this.price = price;
