179e7cfa4257f7164d3b28f6989ec548304390b2
kdkdhoho <hkim4410@naver.com>
2023-04-27 17:22:02 +0900

    feat: 상품 목록 페이지 API 기능 구현

---
+++ src/main/java/cart/controller/ProductController.java
@@ -0,0 +1,25 @@
+package cart.controller;
+
+import cart.dao.ProductDao;
+import cart.entity.ProductEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class ProductController {
+
+    private final ProductDao productDao;
+
+    @GetMapping("/")
+    public String indexPage(final Model model) {
+        List<ProductEntity> products = productDao.findAll();
+
+        model.addAttribute("products", products);
+
+        return "index";
+    }
+}
