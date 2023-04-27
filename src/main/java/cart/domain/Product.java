90c4bd246f4c0ca9d691479a74c45ed18504cf24
kdkdhoho <hkim4410@naver.com>
2023-04-27 10:56:38 +0900

    feat: Product 객체 구현

---
+++ src/main/java/cart/domain/Product.java
@@ -0,0 +1,32 @@
+package cart.domain;
+
+public class Product {
+
+    private final int id;
+    private final String name;
+    private final int price;
+    private final String imageUrl;
+
+    public Product(final int id, final String name, final int price, final String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
